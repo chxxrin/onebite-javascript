d8793a977c8c9a9cef9b21c7aaa0b6d6d2c63c72
박채린 <bewithbomi@gmail.com>
2025-01-11 23:19:45 +0900

    update: section07

--- section04/src/App.jsx
+++ section04/src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <div>
-      안녕 리액트!
-    </div>
+      <h1>안녕 리액트!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ section07/src/App.jsx
@@ -0,0 +1,53 @@
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+import Even from "./components/Even";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const isMount = useRef(false);
+
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+  useEffect(() => {
+    console.log(`count: ${count} / input: ${input}`);
+  }, [count, input]);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        ></input>
+      </section>
+      <section>
+        <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
